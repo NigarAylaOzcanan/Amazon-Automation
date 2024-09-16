7f694fea5d7c240c9c1cb0f6964f5041a8c16d23
Onur GİRGİN <girgin183@gmail.com>
2024-09-16 13:53:48 +0300

    Login procedures successfully completed

--- src/test/java/stepDefinitions/LoginSteps.java
+++ src/test/java/stepDefinitions/LoginSteps.java
@@ -1,4 +1,29 @@
 package stepDefinitions;
 
+import io.cucumber.java.en.*;
+import pages.LoginPOM;
+import utilities.ConfigReader;
+import utilities.GWD;
+
 public class LoginSteps {
+    LoginPOM login = new LoginPOM();
+
+    @Given("Navigate to the Amazon")
+    public void navigateToTheAmazon() {
+        GWD.getDriver().get(ConfigReader.getProperty("URL"));
+    }
+
+    @When("Enter the valid credentials and click the login button")
+    public void enterTheValidCredentialsAndClickTheLoginButton() {
+        login.clickFunction(login.loginButton);
+        login.sendKeysFunction(login.emailBox, ConfigReader.getProperty("e-mail"));
+        login.clickFunction(login.continueButton);
+        login.sendKeysFunction(login.passwordBox, ConfigReader.getProperty("password"));
+        login.clickFunction(login.signInButton);
+    }
+
+    @Then("User should be logged in successfully")
+    public void userShouldBeLoggedInSuccessfully() {
+        login.verifyContainsText(login.accountName, "onur");
+    }
 }
