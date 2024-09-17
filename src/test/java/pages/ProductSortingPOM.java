b3e4f68e498b86fa3525205dd12281672e48b385
Onur GİRGİN <girgin183@gmail.com>
2024-09-17 15:01:17 +0300

    ProductSorting.feature successfully completed

--- src/test/java/pages/ProductSortingPOM.java
+++ src/test/java/pages/ProductSortingPOM.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.support.PageFactory;
 import utilities.GWD;
 import utilities.ReusableMethods;
 
+import java.util.List;
+
 public class ProductSortingPOM extends ReusableMethods {
     public ProductSortingPOM() {
         PageFactory.initElements(GWD.getDriver(), this);
@@ -21,26 +23,55 @@ public class ProductSortingPOM extends ReusableMethods {
     public WebElement gamingLaptops;
 
     @FindBy(css = "[aria-label='MSI'] i")
-    public WebElement MsiSelectBtn;
+    public WebElement msiSelectBtn;
 
     @FindBy(xpath = "(//*[@aria-label='Intel'])[1]//i")
-    public WebElement CpuIntelSelectBtn;
+    public WebElement cpuIntelSelectBtn;
 
     @FindBy(css = "[aria-label='Intel Core i7'] i")
-    public WebElement Corei7SelectBtn;
+    public WebElement coreI7SelectBtn;
 
     @FindBy(css = "[aria-label*='4.0 GHz'] i")
-    public WebElement Ghz4SelectBtn;
+    public WebElement ghz4SelectBtn;
 
-    public WebElement getWebElement(String strElement){
-        switch (strElement){
-            case "computerBtn" : return this.computerBtn;
-            case "laptopBtn" : return this.laptopBtn;
-            case "gamingLaptops" : return this.gamingLaptops;
-            case "MsiSelectBtn" : return this.MsiSelectBtn;
-            case "CpuIntelSelectBtn" : return this.CpuIntelSelectBtn;
-            case "Corei7SelectBtn" : return this.Corei7SelectBtn;
-            case "Ghz4SelectBtn" : return this.Ghz4SelectBtn;
+    @FindBy(css = "[class='a-section a-spacing-small a-spacing-top-small'] span:nth-child(1)")
+    public WebElement totalProductNumber;
+
+    @FindBy(css = "[class='s-result-list-placeholder sg-row aok-hidden']")
+    public WebElement loadingBar;
+
+    @FindBy(css = "[data-cy='price-recipe'] span:nth-child(2)>span:nth-child(1)")
+    public List<WebElement> productPrices;
+
+    @FindBy(css = "[class='a-button-text a-declarative']")
+    public WebElement sortingCriteriaBtn;
+
+    @FindBy(xpath = "(//*[@class='a-nostyle a-list-link']/li)[2]/a")
+    public WebElement fromLowToHigh;
+
+    @FindBy(xpath = "(//a[contains(@id,'s-result-sort-select')])[9]")
+    public WebElement fromHighToLow;
+
+    public WebElement getWebElement(String strElement) {
+        switch (strElement) {
+            case "computerBtn":
+                return this.computerBtn;
+            case "laptopBtn":
+                return this.laptopBtn;
+            case "gamingLaptops":
+                return this.gamingLaptops;
+            case "msiSelectBtn":
+                return this.msiSelectBtn;
+            case "cpuIntelSelectBtn":
+                return this.cpuIntelSelectBtn;
+            case "coreI7SelectBtn":
+                return this.coreI7SelectBtn;
+            case "ghz4SelectBtn":
+                return this.ghz4SelectBtn;
+            case "totalProductNumber":
+                return this.totalProductNumber;
+            case "loadingBar":
+                return this.loadingBar;
 
         }
 
--- src/test/java/stepDefinitions/ProductSortingSteps.java
+++ src/test/java/stepDefinitions/ProductSortingSteps.java
@@ -1,35 +1,113 @@
 package stepDefinitions;
 
+import io.cucumber.datatable.DataTable;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import pages.ProductSortingPOM;
+import utilities.ConfigReader;
+import utilities.GWD;
+import utilities.ReusableMethods;
+import java.time.Duration;
+import java.util.List;
 
 public class ProductSortingSteps {
+    ProductSortingPOM ps = new ProductSortingPOM();
+    public WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(ConfigReader.getIntProperty("explicit.wait")));
+
     @When("User click units on the tab menu")
-    public void userClickUnitsOnTheTabMenu() {
+    public void userClickUnitsOnTheTabMenu(DataTable tabMenuButtons) {
+        List<String> strButtonList = tabMenuButtons.asList(String.class);
+
+        for (int i = 0; i < strButtonList.size(); i++) {
+            WebElement linkWebElement = ps.getWebElement(strButtonList.get(i));
+            ps.jsClick(linkWebElement);
+        }
     }
 
     @And("User hover over on the submenu")
-    public void userHoverOverOnTheSubmenu() {
+    public void userHoverOverOnTheSubmenu(DataTable hoverOverSubmenu) {
+        List<String> strButtonList = hoverOverSubmenu.asList(String.class);
+
+        for (int i = 0; i < strButtonList.size(); i++) {
+            WebElement linkWebElement = ps.getWebElement(strButtonList.get(i));
+            ps.hoverOver(linkWebElement);
+            wait.until(ExpectedConditions.attributeToBe(linkWebElement, "class", "nav-a nav-hasArrow nav-active"));
+        }
     }
 
     @Then("User should be able to verify the total number of products")
     public void userShouldBeAbleToVerifyTheTotalNumberOfProducts() {
+        wait.until(ExpectedConditions.visibilityOf(ps.totalProductNumber));
+        if (ps.totalProductNumber.getText().contains("348")) {
+            ps.verifyContainsText(ps.totalProductNumber, "348");
+        } else {
+            ps.verifyContainsText(ps.totalProductNumber, "10");
+        }
+
+
     }
 
     @And("User click on filtering units")
-    public void userClickOnFilteringUnits() {
+    public void userClickOnFilteringUnits(DataTable filteringButtons) {
+        List<String> strButtonList = filteringButtons.asList(String.class);
+        WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(ConfigReader.getIntProperty("explicit.wait")));
+        for (int i = 0; i < strButtonList.size(); i++) {
+            WebElement linkWebElement = ps.getWebElement(strButtonList.get(i));
+            ps.clickFunction(linkWebElement);
+            //wait.until(ExpectedConditions.attributeToBe(linkWebElement,"class","s-result-list-placeholder sg-row aok-hidden"));
+        }
     }
 
     @When("User sorts products from low to high price")
     public void userSortsProductsFromLowToHighPrice() {
+        ps.clickFunction(ps.sortingCriteriaBtn);
+        ps.clickFunction(ps.fromLowToHigh);
+        ReusableMethods.wait(2);
     }
 
     @Then("User should be able to verify the ranking criterion")
     public void userShouldBeAbleToVerifyTheRankingCriterion() {
+
+        String strFirstPrice = ps.productPrices.get(0).getText().replaceAll("[^0-9,.]", "");
+        double doubleFirstPrice = Double.parseDouble(strFirstPrice);
+        String strSecondProductPrice = ps.productPrices.get(1).getText().replaceAll("[^0-9,.]", "");
+        double doubleSecondProductPrice = Double.parseDouble(strSecondProductPrice);
+
+        if (doubleFirstPrice < doubleSecondProductPrice) {
+            for (int i = 0; i < ps.productPrices.size(); i++) {
+
+                if (i == ps.productPrices.size() - 1) {
+                    break;
+                }
+                String strProductPrice = ps.productPrices.get(i).getText().replaceAll("[^0-9,.]", "");
+                double doubleProductPrice = Double.parseDouble(strProductPrice);
+                String strAfterProductPrice = ps.productPrices.get(i + 1).getText().replaceAll("[^0-9,.]", "");
+                double doubleAfterProductPrice = Double.parseDouble(strAfterProductPrice);
+                Assert.assertTrue(doubleProductPrice < doubleAfterProductPrice, "Product Prices are not from low to high");
+            }
+        } else if (doubleFirstPrice > doubleSecondProductPrice) {
+            for (int i = 0; i < ps.productPrices.size(); i++) {
+                if (i == ps.productPrices.size() - 1) {
+                    break;
+                }
+                String strProductPrice = ps.productPrices.get(i).getText().replaceAll("[^0-9,.]", "");
+                double doubleProductPrice = Double.parseDouble(strProductPrice);
+                String strAfterProductPrice = ps.productPrices.get(i + 1).getText().replaceAll("[^0-9,.]", "");
+                double doubleAfterProductPrice = Double.parseDouble(strAfterProductPrice);
+                Assert.assertTrue(doubleProductPrice > doubleAfterProductPrice, "Product Prices are not from low to high");
+            }
+        }
     }
 
     @And("User sorts products from high to low price")
     public void userSortsProductsFromHighToLowPrice() {
+        ps.clickFunction(ps.sortingCriteriaBtn);
+        ps.clickFunction(ps.fromHighToLow);
+
     }
 }
