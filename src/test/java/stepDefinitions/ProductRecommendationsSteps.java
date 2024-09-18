6c8f77187a62ca0b1ecff5acb101fc870855c6b9
Onur GİRGİN <girgin183@gmail.com>
2024-09-18 14:37:26 +0300

    ProductRecommendations.feature and ProductRecommendationsSteps created class

---
+++ src/test/java/stepDefinitions/ProductRecommendationsSteps.java
@@ -0,0 +1,29 @@
+package stepDefinitions;
+
+import io.cucumber.java.en.*;
+
+public class ProductRecommendationsSteps {
+    @Given("The User navigates down the site to product recommendations")
+    public void theUserNavigatesDownTheSiteToProductRecommendations() {
+    }
+
+    @When("The User clicks on any product in the product list based on purchases")
+    public void theUserClicksOnAnyProductInTheProductListBasedOnPurchases() {
+    }
+
+    @Then("The User performs verification")
+    public void theUserPerformsVerification() {
+    }
+
+    @And("The user comes back to the home page")
+    public void theUserComesBackToTheHomePage() {
+    }
+
+    @And("The User clicks on see viewing history button")
+    public void theUserClicksOnSeeViewingHistoryButton() {
+    }
+
+    @Then("Verifies if the user has navigated to the product list page")
+    public void verifiesIfTheUserHasNavigatedToTheProductListPage() {
+    }
+}
